3a23ac8729503af4dd325a1fb55636676862f586
Mark Kenney <markathon@users.noreply.github.com>
2018-12-10 10:42:28 -0500

    Add files via upload

---
+++ helloWorld/helloWorld/library.cpp
@@ -0,0 +1,14 @@
+#include "library.h"
+#include "pch.h"
+
+#include <iostream>//useful for cout
+
+void test(); //declaration
+
+void test()//definition
+{
+	int a;
+	a = 5;
+	printf("test", "\n");
+	std::cout << "test\n";
+}
---
+++ helloWorld/helloWorld/library.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef library
+#define library
+void test();
+
+
+
+#endif
